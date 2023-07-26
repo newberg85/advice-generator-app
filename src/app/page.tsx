62d47e8eee9433dc73ff2c9787e8502dda052fc5
newberg85 <bergviana12345@gmail.com>
2023-07-26 18:40:38 -0300

    Api and styling have been added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,84 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+//images icon
+import Btnicon from "public/assets/images/icon-dice.svg"
+import Lined from "public/assets/images/pattern-divider-desktop.svg"
+
+const URL_API = "https://api.adviceslip.com/advice"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<{ id: number; advice: string } | null>(null);
+
+  const fetchAllData = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(URL_API);
+      const responseData = await response.json();
       
+
+      if (!responseData.slip || !responseData.slip.advice) {
+        throw new Error("Problema na requisição");
+      }
+
+      setData(responseData.slip);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
+  const fetchNewAdvice = () => {
+    setLoading(true);
+    fetchAllData();
+  };
+
+  return (
+    <main className="w-screen bg-dark-blue flex min-h-screen flex-col items-center justify-center p-2 sm:p-24">
+      <div className="w-full sm:w-96 h-52 rounded-xl bg-dark-grayish-blue flex flex-col gap-3 items-center justify-center text-center p-4 relative">
+        <div>
+          <span className="text-neon-green uppercase text-xs tracking-widest">Adivice #{data?.id}</span>
+          {loading && !data &&
+            <p className="text-light-cyan text-xl">Carregando conselho</p>
+          }
+
+        {data && (
+        <p className="text-light-cyan text-xl" key={data.id}>
+          &quot;{data.advice}&quot;
+        </p>
+      )}
+        </div>
+
+        <div>
+        <Image 
+            src={Lined}
+            width={300}
+            height={50}
+            alt="btn icon"
+        />
+        </div>
+        
+        <div className="flex justify-center">
+          <button className="bg-neon-green w-10
+          h-10 flex items-center justify-center rounded-3xl p-3 absolute -bottom-4 hover:shadow-neon-green shadow-3xl transition delay-150 duration-300 ease-in-out" onClick={fetchNewAdvice}>
+            <Image 
+            src={Btnicon}
+            width={30}
+            height={30}
+            alt="btn icon"
+            />
+          </button>
+        </div>
+      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,6 +8,29 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'neon-green': '#52ffa8',
+      'light-cyan': '#cee3e9',
+      'grayish-blue': '#4e5d73',
+      'dark-grayish-blue': '#323a49',
+      'dark-blue': '#1f2632',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#fafafa',
+      'silver': '#9095a7',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontFamily: {
+      sans: ['var(--font-Manropef)']
+    },
+    boxShadow: {
+      '3xl': '0 0px 20px 0px rgba(0, 0, 0, 0.3)',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
